40bf44d2334609bf5c534f13222fafdf2db4716f
Aleksey <alyushnin@yandex.ru>
2017-11-20 19:03:19 +0300

    make

---
+++ js/LyushninHW5_1.js
@@ -0,0 +1,17 @@
+window.onload = function(){
+var chessWrap = document.querySelector('.ch-wrap');
+
+var i = 0, count = 0;
+  
+while (count < 8 * 8) {
+  var item = document.createElement('div');
+  chessWrap.appendChild(item);
+  item.classList.add('ch-item');
+
+  if (i && i % 2)
+    item.classList.add('ch-black')
+
+  i += ((i + 2) % 9) ? 1 : 2;
+  count++;
+}
+}
