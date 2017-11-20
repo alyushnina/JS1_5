c0e5f32e5c7627c45e501bb5fcba89e6f1d5fe7a
Aleksey <alyushnin@yandex.ru>
2017-11-20 21:30:24 +0300

    цикл на див
    Пробовал переделать цикл на дивы

--- js/LyushninHW5_1.js
+++ js/LyushninHW5_1.js
@@ -1,17 +1,21 @@
 window.onload = function(){
-var chessWrap = document.querySelector('.ch-wrap');
+	var chessWrap = document.querySelector('.ch-wrap');
 
-var i = 0, count = 0;
-  
-while (count < 8 * 8) {
-  var item = document.createElement('div');
-  chessWrap.appendChild(item);
-  item.classList.add('ch-item');
+	var divBoard = document.createElement('div');
+	divBoard.className = 'board';
+	document.body.appendChild(divBoard);
+	
+	var i = 0, count = 0;
 
-  if (i && i % 2)
-    item.classList.add('ch-black')
+	while (count < 8 * 8) {
+		var item = document.createElement('div');
+		chessWrap.appendChild(item);
+		item.classList.add('ch-item');
 
-  i += ((i + 2) % 9) ? 1 : 2;
-  count++;
-}
+		if (i && i % 2)
+			item.classList.add('ch-black')
+
+		i += ((i + 2) % 9) ? 1 : 2;
+		count++;
+	}
 }
